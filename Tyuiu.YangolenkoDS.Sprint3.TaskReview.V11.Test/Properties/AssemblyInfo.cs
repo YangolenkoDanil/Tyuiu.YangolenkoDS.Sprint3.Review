6f470461fe42e1c3ff01b0f97771308e55ac56e7
angol <angol@DESKTOP-MTNAJUS>
2023-11-01 10:48:43 +0500

    Add project files.

---
+++ Tyuiu.YangolenkoDS.Sprint3.TaskReview.V11.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.YangolenkoDS.Sprint3.TaskReview.V11.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.YangolenkoDS.Sprint3.TaskReview.V11.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("2a230d0e-b098-4680-bb09-bf3ea6bda691")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.YangolenkoDS.Sprint3.TaskReview.V11/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.YangolenkoDS.Sprint3.TaskReview.V11.Lib;
+
+namespace Tyuiu.YangolenkoDS.Sprint3.TaskReview.V11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #3 | Выполнил: Янголенко Д. С. | АСОиУб-23-1";
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* Спринт #3                                                                         *");
+            Console.WriteLine("* Тема: Review                                                                      *");
+            Console.WriteLine("* Задание TaskReview                                                                *");
+            Console.WriteLine("* Вариант #11                                                                       *");
+            Console.WriteLine("* Выполнил: Янголенко Данил Сергеевич | АСОиУб-23-1                                 *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+            Console.WriteLine("* Написать программу, которая выводит таблицу значений функции                      *");
+            Console.WriteLine("*                                                                                   *");
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+            Console.WriteLine("*************************************************************************************");
+
+            int startValue = -5;
+            int stopValue = 5;
+
+            int len = ds.GetMassFunction(startValue, stopValue).Length;
+
+            double[] valueArray;
+            valueArray = new double[len];
+
+            valueArray = ds.GetMassFunction(startValue, stopValue);
+
+
+            Console.WriteLine("Старт шага - " + startValue);
+            Console.WriteLine("Конец шага - " + stopValue);
+
+
+            Console.WriteLine("*************************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+            Console.WriteLine("*************************************************************************************");
+
+            Console.WriteLine("+---------+---------+");
+            Console.WriteLine("|    X    |   F(x)  |");
+            Console.WriteLine("+---------+---------+");
+
+            for (int i = 0; i <= len - 1; i++)
+            {
+                Console.WriteLine("| {0,5:d}   | {1, 6:f2}  |", startValue, valueArray[i]);
+                startValue++;
+            }
+            Console.WriteLine("+---------+---------+");
+            Console.ReadKey();
+        }
+    }
+}
